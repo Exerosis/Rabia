2d26df8c4ab4cad5c492af69285198ccca1ec273
Exerosis <exerosis@gmail.com>
2022-09-27 04:33:34 -0400

    ~ Testing initial rabia implementation.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,12 @@ plugins { kotlin("jvm").version("1.7.10") }
 group = "com.github.exerosis.rabia"
 version = "1.0.0"
 
-repositories { mavenCentral() }
+repositories {
+    maven("https://repo1.maven.org/maven2/")
+    mavenLocal();maven("https://jitpack.io")
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("com.github.exerosis.mynt:Mynt:1.0.10")
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,132 @@
+import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import java.net.InetAddress
+import java.net.InetAddress.*
+import java.net.InetSocketAddress
+import java.net.NetworkInterface
+import java.net.StandardProtocolFamily
+import java.net.StandardProtocolFamily.*
+import java.net.StandardSocketOptions.*
+import java.nio.ByteBuffer
+import java.nio.channels.DatagramChannel
+import java.util.concurrent.PriorityBlockingQueue
+import kotlin.experimental.or
+import kotlin.random.Random
+import kotlin.text.Charsets.UTF_8
 
+const val PORT = 5000
+const val BROADCAST = "230.0.0.0"
+const val SIZE = 65
+const val COUNT = 5
 
+const val OP_PROPOSE = 1
+const val OP_STATE = 2
+const val OP_VOTE = 3
+const val OP_LOST = 4
+
+const val STATE_ZERO = (OP_STATE shl 6).toByte()
+const val STATE_ONE = (OP_STATE shl 6 or 32).toByte()
+
+const val VOTE_ZERO = (OP_VOTE shl 6).toByte()
+const val VOTE_ONE = (OP_VOTE shl 6 or 32).toByte()
+const val VOTE_LOST = (OP_LOST shl 6).toByte()
+
+suspend fun Node(
+    n: Int, f: Int, random: Random,
+    commit: suspend (ByteArray?) -> (Unit),
+    commands: suspend () -> (ByteArray)
+) = withContext(IO) {
+    val channel = DatagramChannel.open(INET)
+    val loopback = NetworkInterface.getByName("lo")
+    channel.setOption(SO_REUSEADDR, true)
+    channel.setOption(IP_MULTICAST_LOOP, true)
+    channel.setOption(IP_MULTICAST_IF, loopback)
+    channel.bind(InetSocketAddress("127.0.0.1", PORT))
+    channel.join(getByName(BROADCAST), loopback)
+    val broadcast = InetSocketAddress(BROADCAST, PORT)
+    val buffer = ByteBuffer.allocateDirect(SIZE)
+    val proposals = Array(COUNT) { ByteArray(SIZE) }
+    val majority = (n / 2) + 1
+    var index: Int
+    fun phase(
+        p: Byte, state: Byte,
+        common: ByteArray?
+    ): ByteArray? {
+        buffer.clear().put(state or p)
+        channel.send(buffer.flip(), broadcast)
+        var zero = 0; var one = 0; var lost = 0;
+        while ((zero + one) < (n - f)) {
+            channel.receive(buffer.clear())
+            when (buffer.get(0)) {
+                STATE_ONE or p -> ++one
+                STATE_ZERO or p -> ++zero
+            }
+        }
+        buffer.clear().put(when {
+            zero >= majority -> VOTE_ZERO
+            one >= majority -> VOTE_ONE
+            else -> VOTE_LOST
+        } or p)
+        channel.send(buffer.flip(), broadcast)
+        zero = 0; one = 0
+        while ((zero + one + lost) < (n - f)) {
+            channel.receive(buffer.clear())
+            when (buffer.get(0)) {
+                VOTE_ONE or p -> ++one
+                VOTE_ZERO or p -> ++zero
+                VOTE_LOST or p -> ++lost
+            }
+        }
+        return if (zero >= f + 1) null
+        else if (one >= f + 1) common
+        else phase((p + 1).toByte(), when {
+            zero > 0 -> STATE_ZERO
+            one > 0 -> STATE_ONE
+            else -> if (random.nextBoolean())
+                STATE_ZERO else STATE_ONE
+        }, common)
+    }
+    while (channel.isOpen) {
+        //Send proposals
+        val command = commands()
+        assert(command.size <= 64) { "Message too large!" }
+        val propose = (OP_PROPOSE shl 6) or command.size
+        buffer.clear().put(propose.toByte()).put(command)
+        channel.send(buffer.flip(), broadcast)
+        index = 0
+        while (index < (n - f)) {
+            channel.receive(buffer.clear())
+            buffer.get(0, proposals[index])
+            val mask = OP_PROPOSE shl 6
+            if (proposals[index][0].toInt() and mask != 0)
+                ++index
+        }
+        val request = proposals.find {
+            proposals.count(it::contentEquals) >= majority
+        }?.let { it.copyOfRange(1, (it[0].toInt() and 0b111111) + 1) }
+        val state = if (request != null) STATE_ONE else STATE_ZERO
+        commit(phase(0, state, request))
+    }
+}
+
+fun main() = runBlocking {
+    val random = Random(0L)
+    val n = 6; val f = 2
+    val nodes = (0 until n).map { i ->
+        PriorityBlockingQueue<String>().apply {
+            launch(Default) { try {
+                Node(n, f, random, {
+                    println("Node: $i - ${it?.toString(UTF_8)}")
+                }, { take().toByteArray(UTF_8) })
+            } catch (reason: Throwable) {
+                reason.printStackTrace()
+            } }
+        }
+    }
+    nodes.forEach { it.offer("hello world") }
+    println("Done!")
+}
