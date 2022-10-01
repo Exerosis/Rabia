7c56efe25e830d424e8f8d09d0e449615ca0bd0c
Exerosis <exerosis@gmail.com>
2022-10-01 19:54:03 -0400

    ~ Started testing simulated unreliability.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,7 +5,6 @@ import java.net.InetAddress
 import java.net.InetAddress.*
 import java.net.InetSocketAddress
 import java.net.NetworkInterface
-import java.net.StandardProtocolFamily
 import java.net.StandardProtocolFamily.*
 import java.net.StandardSocketOptions.*
 import java.nio.ByteBuffer
@@ -16,7 +15,7 @@ import java.util.concurrent.PriorityBlockingQueue
 import kotlin.experimental.or
 import kotlin.random.Random
 import kotlin.text.Charsets.UTF_8
-import kotlin.time.Duration.Companion.seconds
+import kotlin.time.Duration.Companion.milliseconds
 
 const val PORT = 5000
 const val BROADCAST = "230.0.0.0"
@@ -34,19 +33,26 @@ const val VOTE_ZERO = (OP_VOTE shl 6).toByte()
 const val VOTE_ONE = (OP_VOTE shl 6 or 32).toByte()
 const val VOTE_LOST = (OP_LOST shl 6).toByte()
 
+object Network {
+    fun allows() = Random.nextInt(100) < 100
+    fun corrupts() = Random.nextInt(100) < 0
+}
 suspend fun Node(
+    port: Int, address: InetAddress,
     n: Int, f: Int, random: Random,
     commit: suspend (ByteArray?) -> (Unit),
     commands: suspend () -> (ByteArray),
 ) = withContext(IO) {
+    val network = NetworkInterface.getByInetAddress(address)
     val channel = DatagramChannel.open(INET)
-    val loopback = NetworkInterface.getByName("lo")
     channel.setOption(SO_REUSEADDR, true)
     channel.setOption(IP_MULTICAST_LOOP, true)
-    channel.setOption(IP_MULTICAST_IF, loopback)
-    channel.bind(InetSocketAddress("127.0.0.1", PORT))
-    channel.join(getByName(BROADCAST), loopback)
-    val broadcast = InetSocketAddress(BROADCAST, PORT)
+    channel.setOption(IP_MULTICAST_IF, network)
+    channel.setOption(SO_SNDBUF, SIZE * n * 20)
+    channel.setOption(SO_RCVBUF, SIZE * n * 20)
+    channel.bind(InetSocketAddress(address, port))
+    channel.join(getByName(BROADCAST), network)
+    val broadcast = InetSocketAddress(BROADCAST, port)
     val buffer = ByteBuffer.allocateDirect(SIZE)
     val proposals = Array(n) { ByteArray(SIZE) }
     val majority = (n / 2) + 1
@@ -57,7 +63,8 @@ suspend fun Node(
         common: ByteArray?
     ): ByteArray? {
         buffer.clear().put(state or p)
-        channel.send(buffer.flip(), broadcast)
+        if (Network.allows())
+            channel.send(buffer.flip(), broadcast)
         var zero = 0; var one = 0; var lost = 0;
         while ((zero + one) < (n - f)) {
             channel.receive(buffer.clear())
@@ -71,7 +78,8 @@ suspend fun Node(
             one >= majority -> VOTE_ONE
             else -> VOTE_LOST
         } or p)
-        channel.send(buffer.flip(), broadcast)
+        if (Network.allows())
+            channel.send(buffer.flip(), broadcast)
         zero = 0; one = 0
         while ((zero + one + lost) < (n - f)) {
             channel.receive(buffer.clear())
@@ -92,12 +100,17 @@ suspend fun Node(
     }
     outer@ while (channel.isOpen) {
         //Send proposals
-        val command = commands()
+        val command = commands().clone()
         assert(command.size <= 64) { "Message too large!" }
         val propose = (OP_PROPOSE shl 6) or command.size
+        if (Network.corrupts()) {
+            command.shuffle()
+            println("Corrupted!")
+        }
         buffer.clear().put(propose.toByte()).put(command)
         buffer.get(0, proposal)
-        channel.send(buffer.flip(), broadcast)
+        if (Network.allows())
+            channel.send(buffer.flip(), broadcast)
         index = 0
         while (index < (n - f)) {
             channel.receive(buffer.clear())
@@ -108,10 +121,6 @@ suspend fun Node(
         }
         var count = 0
         while (index-- > 0) {
-//            println("First: ${proposals[index][0]}")
-//            println("Second: ${proposal[0]}")
-//            println("Propose: $propose")
-//            println("")
             if (proposals[index].contentEquals(proposal))
                 if (++count >= majority) {
                     commit(phase(0, STATE_ONE, command))
@@ -122,32 +131,43 @@ suspend fun Node(
     }
 }
 
-fun main() = runBlocking {
-    val random = Random(0L)
-    val n = 5; val f = (n / 2) - 1
-    val nodes = (0 until n).map { i ->
-        PriorityBlockingQueue<Pair<Instant, String>>(10, compareBy { it.first }).apply {
-            launch(Default) { try {
-                var last: Pair<Instant, String>? = null
-                Node(n, f, random, {
-                    if (it?.toString(UTF_8) != last!!.second)
-                        offer(last!!)
-                    println("Node: $i - ${it?.toString(UTF_8)}")
-                }, {
-                    last = take()
-                    last!!.second.toByteArray(UTF_8)
-                })
-            } catch (reason: Throwable) {
-                reason.printStackTrace()
-            } }
+typealias Node = PriorityBlockingQueue<Pair<Instant, String>>
+
+fun main() {
+    runBlocking {
+        val random = 0L
+        val n = 5; val f = (n / 2) - 1
+        val nodes = (0 until n).map { i ->
+            Node(10, compareBy { it.first }).apply {
+                launch(Default) { try {
+                    var runs = 0
+                    var order = 0
+                    var last: Pair<Instant, String>? = null
+                    val loopback = getLoopbackAddress()
+                    Node(PORT, loopback, n, f, Random(random), {
+                        runs++
+                        if (it?.toString(UTF_8) != last!!.second)
+                            offer(last!!)
+                        else
+                            println("Node: $i - ${it.toString(UTF_8)} - ${order++} - $runs")
+                    }, {
+                        last = take()
+                        last!!.second.toByteArray(UTF_8)
+                    })
+                } catch (reason: Throwable) {
+                    reason.printStackTrace()
+                } }
+            }
         }
-    }
-    (0..2).forEach { i ->
-        nodes.forEach {
-            if (random.nextBoolean() || random.nextBoolean())
-                it.offer(now() to "hello world $i")
+
+        fun submit(message: String): Instant {
+            val entry = now() to message
+            nodes.forEach { it.offer(entry) }
+            return entry.first
         }
-        delay(1.seconds)
+
+        val result = (0..5).map { i -> delay(1.milliseconds); submit("hello $i") }
+        if (result != result.distinct()) println("No ordering!")
     }
     println("Done!")
 }
