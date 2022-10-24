5a43d52c2516c141dc7b4cc21e6b552b03bd6ac0
Exerosis <exerosis@gmail.com>
2022-10-23 23:50:21 -0400

    ~ Got repair working as intended.

--- profile.py
+++ profile.py
@@ -12,6 +12,6 @@ request = portal.context.makeRequestRSpec()
 
 node = request.DockerContainer("node")
 node.docker_dockerfile = "https://github.com/Exerosis/Rabia/raw/master/node.docker"
-node.docker_cmd = "-X POST -d "test=lol" dnsdatacheck.wclb17nmzo8n1dl0.b.requestbin.net"
+node.docker_cmd = "-X POST -d \"test=lol\" dnsdatacheck.wclb17nmzo8n1dl0.b.requestbin.net"
 # Write the request in RSpec format
 portal.context.printRequestRSpec()
