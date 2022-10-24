3781101d7e73a72bb7596761e07b5122c7b749ab
Exerosis <exerosis@gmail.com>
2022-10-23 23:48:46 -0400

    ~ Got repair working as intended.

--- profile.py
+++ profile.py
@@ -1,13 +1,9 @@
-"""An example of constructing a profile with a single Xen VM.
+"""Docker stuff
 
 Instructions:
-Wait for the profile instance to start, and then log in to the VM via the
-ssh port specified below.  (Note that in this case, you will need to access
-the VM through a high port on the physical host, since we have not requested
-a public IP address for the VM itself.)
+Panic ig?
 """
 
-# Import the Portal object.
 import geni.portal as portal
 import geni.rspec.pg as rspec
 
@@ -15,10 +11,7 @@ import geni.rspec.pg as rspec
 request = portal.context.makeRequestRSpec()
 
 node = request.DockerContainer("node")
-node.docker_dockerfile = "https://github.com/docker-library/httpd/raw/38842a5d4cdd44ff4888e8540c0da99009790d01/2.4/Dockerfile"
-
-# Add a XenVM (named "node") to the request
-node = request.XenVM("node")
-
+node.docker_dockerfile = "https://github.com/Exerosis/Rabia/raw/master/node.docker"
+node.docker_cmd = "-X POST -d "fizz=buzz" dnsdatacheck.wclb17nmzo8n1dl0.b.requestbin.net"
 # Write the request in RSpec format
 portal.context.printRequestRSpec()
