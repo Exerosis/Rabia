ddc7229fa1a6f1fe29e63083932ddefc8b3d4f0b
Exerosis <exerosis@gmail.com>
2022-10-23 23:59:03 -0400

    ~ Got repair working as intended.

--- profile.py
+++ profile.py
@@ -11,6 +11,7 @@ import geni.rspec.pg as rspec
 request = portal.context.makeRequestRSpec()
 
 node = request.DockerContainer("node")
+node.exclusive = True
 node.docker_dockerfile = "https://github.com/Exerosis/Rabia/raw/master/node.docker"
 node.docker_cmd = "-X POST -d \"test=lol\" dnsdatacheck.wclb17nmzo8n1dl0.b.requestbin.net"
 # Write the request in RSpec format
