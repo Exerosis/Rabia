df78cf7ad591f31d44397ff81f6808d91bd4529e
Exerosis <exerosis@gmail.com>
2022-10-26 00:57:55 -0400

    ~ Testing UDP RTT

--- profile.py
+++ profile.py
@@ -12,7 +12,7 @@ request = portal.context.makeRequestRSpec()
 lan = request.LAN()
 for i in [1, 2, 3, 4, 5]:
     node = request.RawPC("node-" + str(i))
-    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:docker-alpine3-std"
+    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
     interface = node.addInterface("if" + str(i))
     interface.component_id = "eth" + str(i)
     interface.addAddress(rspec.IPv4Address("192.168.1." + str(i), "255.255.255.0"))
