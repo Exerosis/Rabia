5d011612905d458a4ae3a767c6c635bbe3f01706
Exerosis <exerosis@gmail.com>
2022-11-06 19:41:46 -0500

    ~ Testing on my laptop.

--- src/main/kotlin/com/github/exerosis/rabia/Main.kt
+++ src/main/kotlin/com/github/exerosis/rabia/Main.kt
@@ -23,41 +23,39 @@ suspend fun log(message: String) {
     }
 }
 
-//fun main() = runBlocking(dispatcher) {
-//    val hostname = getLocalHost().hostName.split('.')[0]
-//    println("Hostname: $hostname")
-//    val main = hostname == "DESKTOP-NJ3CTN8"
-//    val current =  if (main) "192.168.10.38" else "192.168.10.54"
-//    val other = if (main) "192.168.10.54" else "192.168.10.38"
-//
-//    val address = getByName(current)
-//    val nodes = arrayOf(InetSocketAddress(other, 1000))
-//
-//    val network = NetworkInterface.getByInetAddress(address)
-//    println("Interface: ${network.displayName}")
-//
-//    for (i in 0 until (if (main) 2 else 2)) { //(if (main) 2 else 3)
-//        //create a node that takes messages on 1000
-//        //and runs weak mvc instances on 2000-2002
-//        val processed = AtomicInteger(0)
-//        var index = 0
-//        SMR(4, nodes, address, 1000 + i, 2000 + (i * 4), 3000) {
-//            processed.incrementAndGet()
-////            if ("$index" != it) error("IDk why this is happening :D")
-//            println("${index++}: $it")
-//        }
-//
-//        launch {
-//            while (isActive) {
-//                val count = processed.getAndSet(0)
-//                println("Operations: $count - ${count / 15.0}/s")
-//                delay(15.seconds)
-//            }
-//        }
-//    }
-//}
+fun run() = runBlocking(dispatcher) {
+    val hostname = getLocalHost().hostName.split('.')[0]
+    println("Hostname: $hostname")
+    val main = hostname == "DESKTOP-NJ3CTN8"
+    val current =  if (main) "192.168.10.38" else "192.168.10.54"
+    val other = if (main) "192.168.10.54" else "192.168.10.38"
 
-fun main() = test()
+    val address = getByName(current)
+    val nodes = arrayOf(InetSocketAddress(other, 1000))
+
+    val network = NetworkInterface.getByInetAddress(address)
+    println("Interface: ${network.displayName}")
+
+    for (i in 0 until (if (main) 2 else 2)) { //(if (main) 2 else 3)
+        //create a node that takes messages on 1000
+        //and runs weak mvc instances on 2000-2002
+        val processed = AtomicInteger(0)
+        var index = 0
+        SMR(4, nodes, address, 1000 + i, 2000 + (i * 4), 3000) {
+            processed.incrementAndGet()
+//            if ("$index" != it) error("IDk why this is happening :D")
+            println("${index++}: $it")
+        }
+
+        launch {
+            while (isActive) {
+                val count = processed.getAndSet(0)
+                println("Operations: $count - ${count / 15.0}/s")
+                delay(15.seconds)
+            }
+        }
+    }
+}
 fun test() = runBlocking(dispatcher) {
     val address = getByName("192.168.10.254")
     println(address)
@@ -72,3 +70,24 @@ fun test() = runBlocking(dispatcher) {
     channel.send(buffer, InetSocketAddress(BROADCAST, 1000))
     println("Sent!")
 }
+fun test2() = runBlocking(dispatcher) {
+    val hostname = getLocalHost().hostName.split('.')[0]
+    println("Hostname: $hostname")
+    val main = hostname == "DESKTOP-NJ3CTN8"
+    val current =  if (main) "192.168.10.38" else "192.168.10.254"
+    val address = getByName(current)
+    println(address)
+    val channel = UDP(address, 1000, 1000)
+    if (main) {
+        val buffer = ByteBuffer.allocateDirect(12)
+        channel.receive(buffer)
+        println("Got data!")
+    } else {
+        val buffer = ByteBuffer.allocateDirect(12)
+        buffer.putInt(10).putLong(15L).flip()
+        channel.send(buffer, InetSocketAddress(BROADCAST, 1000))
+        println("Sent!")
+    }
+}
+
+fun main() = test2()
