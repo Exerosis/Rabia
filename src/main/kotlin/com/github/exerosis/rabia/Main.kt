511823404384b49138b2b7745ea3815f443d75e2
Exerosis <exerosis@gmail.com>
2022-12-11 20:16:10 -0500

    ~ Added a timeout.

--- src/main/kotlin/com/github/exerosis/rabia/Main.kt
+++ src/main/kotlin/com/github/exerosis/rabia/Main.kt
@@ -16,8 +16,8 @@ import kotlin.streams.asSequence
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.ExperimentalTime
 
-const val DEBUG = false
-const val WARN = false
+const val DEBUG = true
+const val WARN = true
 const val SERVER = false
 
 fun run() {
