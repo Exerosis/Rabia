43a43be556109cbb935f017764ff372a5b6fd5b6
Exerosis <exerosis@gmail.com>
2022-12-16 05:44:11 -0500

    ~ Added a timeout.

--- src/main/kotlin/com/github/exerosis/rabia/Main.kt
+++ src/main/kotlin/com/github/exerosis/rabia/Main.kt
@@ -3,58 +3,38 @@
 package com.github.exerosis.rabia
 
 import kotlinx.coroutines.runBlocking
+import java.net.InetAddress
 import java.net.InetAddress.getByName
-import java.net.InetSocketAddress
 import java.net.NetworkInterface
-import java.util.concurrent.atomic.AtomicInteger
 import kotlin.streams.asSequence
 import kotlin.time.ExperimentalTime
 
 const val INFO = true
 const val DEBUG = false
 const val WARN = true
-const val SERVER = false
 
 fun run() {
     runBlocking(dispatcher) {
-        println("Hostname: $hostname")
-        println("Current: ${current()}")
-        println("Other: ${other()}")
+        val hostname = InetAddress.getLocalHost().hostName.split('.')[0]
         val address = NetworkInterface.networkInterfaces().asSequence().flatMap {
             it.inetAddresses.asSequence()
-        }.find { println(it); "192.168.1" in it.toString() }!!
-        println(address)
-//    val repairs = arrayOf(
-//        InetSocketAddress(other(), 2000),
-//        InetSocketAddress(other(), 2001)
-//    )
-//    val nodes = arrayOf(
-//        InetSocketAddress(current(), 3000),
-//        InetSocketAddress(current(), 3004),
-//        InetSocketAddress(other(), 3000),
-//        InetSocketAddress(other(), 3004)
-//    )
-        val repairs = arrayOf(
-            InetSocketAddress("192.168.1.1", 2000),
-            InetSocketAddress("192.168.1.2", 2000),
-            InetSocketAddress("192.168.1.3", 2000),
-//        InetSocketAddress("192.168.1.4", 2000),
-        )
+        }.find { "192.168.1" in it.toString() }!!
+        val network = NetworkInterface.getByInetAddress(address)
+        println("Hostname: $hostname")
+        println("Address: $address")
+        println("Interface: ${network.displayName}")
+
         val nodes = arrayOf(
             getByName("192.168.1.1"),
             getByName("192.168.1.2"),
             getByName("192.168.1.3"),
         )
 
-        val network = NetworkInterface.getByInetAddress(address)
-        println("Interface: ${network.displayName}")
-        val processed = AtomicInteger(0)
-        var index = 0
+
         SMR(3, address, nodes,
             queue=1000, repair=2000,
             pipes=intArrayOf(3000, 4000)
         ) {
-            processed.incrementAndGet()
 //            if ("$index" != it) error("IDk why this is happening :D")
 //            println("${index++}: $it")
         }
