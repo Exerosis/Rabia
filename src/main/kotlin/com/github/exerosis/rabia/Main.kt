7e97845f98391fa981c42572a5015efcec74d01f
Exerosis <exerosis@gmail.com>
2022-12-18 12:15:55 -0500

    ~ Added a timeout.

--- src/main/kotlin/com/github/exerosis/rabia/Main.kt
+++ src/main/kotlin/com/github/exerosis/rabia/Main.kt
@@ -33,7 +33,7 @@ fun run() {
 
         SMR(3, address, nodes,
             queue=1000, repair=2000,
-            pipes=IntArray(40) { 3000 + (it * 100) }
+            pipes=IntArray(15) { 3000 + (it * 100) }
         ) {
 //            println("${index++}: $it")
         }
