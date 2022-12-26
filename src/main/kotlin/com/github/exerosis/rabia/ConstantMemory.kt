dad611b8104945402fe546091d88068e1cf045e2
Exerosis <exerosis@gmail.com>
2022-12-26 18:47:03 -0500

    ~ Trying to implement catchup.

--- src/main/kotlin/com/github/exerosis/rabia/ConstantMemory.kt
+++ src/main/kotlin/com/github/exerosis/rabia/ConstantMemory.kt
@@ -1,3 +1,10 @@
 package com.github.exerosis.rabia
 
-
+class Theory(logs: Int, majority: Int) {
+    val proposals = LongArray(logs * majority)
+    val statesZero = ByteArray(logs * 256)
+    val statesOne = ByteArray(logs * 256)
+    val votesZero = ByteArray(logs * 256)
+    val votesOne = ByteArray(logs * 256)
+    val votesLost = ByteArray(logs * 256)
+}
