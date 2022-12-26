8f701e1e036dfcbfde18e63223bfa36a55ad8b16
Exerosis <exerosis@gmail.com>
2022-12-26 16:54:25 -0500

    ~ Trying to implement catchup.

--- src/main/kotlin/com/github/exerosis/rabia/Main.kt
+++ src/main/kotlin/com/github/exerosis/rabia/Main.kt
@@ -33,7 +33,7 @@ fun run() {
 
         SMR(3, address, nodes,
             queue=2000, repair=2001,
-            pipes=IntArray(15) { 3000 + (it * 100) }
+            pipes=IntArray(30) { 3000 + (it * 100) }
         ) {
 //            println("${index++}: $it")
         }
