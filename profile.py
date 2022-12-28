859ae75db5141a7b22d7fe90b63bbf49712d41fd
Exerosis <exerosis@gmail.com>
2022-12-27 21:38:00 -0500

    ~ Reimplemented

--- profile.py
+++ profile.py
@@ -10,7 +10,7 @@ import geni.rspec.pg as rspec
 request = portal.context.makeRequestRSpec()
 
 lan = request.LAN()
-for i in [1, 2, 3, 4, 5]:
+for i in [1, 2, 3]: #, 4, 5
     node = request.RawPC("node-" + str(i))
     node.hardware_type = "d430" #d710
     node.disk_image = "urn:publicid:IDN+emulab.net+image+HyflowTM:Rabia-Kotlin-UDP.base"
