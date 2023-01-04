63bee48dc9b1419b0f66ac8593c7d4d1b1571233
Exerosis <exerosis@gmail.com>
2023-01-03 21:50:13 -0500

    ~ Networking testing.

--- src/main/kotlin/com/github/exerosis/rabia/Main.kt
+++ src/main/kotlin/com/github/exerosis/rabia/Main.kt
@@ -18,7 +18,7 @@ const val WARN = true
 const val COUNT = 10_000_000
 const val AVERAGE = 10_000
 
-val executor = Executors.newFixedThreadPool(8) as ThreadPoolExecutor
+val executor = Executors.newCachedThreadPool() as ThreadPoolExecutor
 val dispatcher = executor.asCoroutineDispatcher()
 
 fun run() {
