b0f9dab7eabbb710d68d4977f35dcb008aa860a0
Exerosis <exerosis@gmail.com>
2023-01-04 00:40:12 -0500

    ~ Networking testing.

--- src/main/kotlin/com/github/exerosis/rabia/Main.kt
+++ src/main/kotlin/com/github/exerosis/rabia/Main.kt
@@ -48,6 +48,8 @@ fun run() {
         //15 - dies (2)
         //16 - 68_000 - 35_000 (2)
         //17 - dies (1)
+        //18 -
+        //20 - dies (1)
         SMR(3, address, nodes,
             queue=2000, repair=2001,
             pipes=IntArray(18) { 3000 + (it * 100) }
