b0c4dc677388fe00e862f19705b516f7f9ba3ad9
Exerosis <exerosis@gmail.com>
2023-11-08 03:01:39 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -13,7 +13,7 @@ lan = request.LAN()
 for i in [1, 2, 3, 4, 5]: #, 4, 5
     node = request.RawPC("node-" + str(i))
     node.hardware_type = "d430" #d430
-    node.disk_image = "urn:publicid:IDN+emulab.net+image+HyflowTM:etcd-test.node-1"
+    node.disk_image = "urn:publicid:IDN+emulab.net+image+HyflowTM:image-with-go-ycsb"
 #     node.addService(RSpec.Execute(
 #         shell="sh", command="sudo /local/scripts/startup.sh"
 #     ))
