58f93aa1f84ede115dbcbb3f613d5be591b95bd0
Exerosis <exerosis@gmail.com>
2024-01-10 05:50:36 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -14,12 +14,6 @@ for i in [1, 2, 3, 4, 5, 6]: #, 4, 5
     node = request.RawPC("node-" + str(i))
     node.hardware_type = "d430" #d710
     node.disk_image = "urn:publicid:IDN+emulab.net+image+HyflowTM:rs-rabia-testing"
-#     node.addService(RSpec.Execute(
-#         shell="sh", command="sudo /local/scripts/startup.sh"
-#     ))
-    interface = node.addInterface("if" + str(i))
-    interface.component_id = "eth" + str(i)
-    interface.addAddress(rspec.IPv4Address("192.168.1." + str(i), "255.255.255.0"))
     lan.addInterface(interface)
 
 portal.context.printRequestRSpec()
